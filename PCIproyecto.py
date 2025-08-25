889bf5a49b14cacdbf179bf4924545e58ae1e9e5
almaazann <A01715142@tec.mx>
2025-08-24 22:49:40 -0600

    Algoritmo
    Archivo con el algoritmo a seguir

--- PCIproyecto.py
+++ PCIproyecto.py
@@ -1,1 +1,25 @@
 #Aqui se realizara el proyecto de Pensamiento computacional 
+
+"""Algoritmo
+
+ENTRADAS:
+-Pedir al usuario determinar si es una ecuacion de primer o segundo grado
+-En caso de ser de primer grado, pedir al usuario como está escrita la ecuacion
+-En caso de ser de segundo grado, pedir al usuario que forma tiene la ecuacion y pedir
+los coeficientes
+
+PROCESOS
+1.-PEDIR si es ecuacion de primer o segundo grado
+2.-SI es de primer grado:
+    2.1.-PEDIR al usuario la forma en la que está escrita la ecuació
+    2.2.-Llevar a cabo el calculo del despeje de X
+    2.3.-MOSTRAR el valor de X
+3.-SINO:
+    3.1.-PEDIR al usuario la forma que tiene la ecuación
+    3.2.-PEDIR al usuario los coeficientes
+    3.3.-Llevar a cabo el calculo para el despeje de X
+    3.4.-MOSTRAR el valor de las raíces
+
+
+SALIDAS
+Valor de X o de las raíces/0's/X1 y x2"""
